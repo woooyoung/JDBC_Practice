308e9c45da68d51190996f059e7d772a8906ad1b
woooyoung <axdsw121@gmail.com>
2022-08-29 13:22:17 +0900

    게시물 작성시 DB에 INSERT

--- src/com/KoreaIT/JP/Main.java
+++ src/com/KoreaIT/JP/Main.java
@@ -1,5 +1,9 @@
 package com.KoreaIT.JP;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -38,11 +42,53 @@ public class Main {
 
 				Article article = new Article(id, title, body);
 				articles.add(article);
+
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JDBCTest?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW()";
+					sql += ", updateDate = NOW()";
+					sql += ", title = '" + title + "'";
+					sql += ", `body` = '" + body + "';";
+
+					System.out.println(sql);
+					pstmt = conn.prepareStatement(sql);
+
+					int affectedRows = pstmt.executeUpdate();
+
+					System.out.println(affectedRows + "열에 적용됨");
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+
 				lastArticleId++;
 
-				System.out.println(article);
-
-				continue;
 			} else if (cmd.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
 
