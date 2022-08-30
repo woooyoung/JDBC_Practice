a875514f6d28fc084acefdf55c3fbf3fae5b1edf
woooyoung <axdsw121@gmail.com>
2022-08-30 15:11:43 +0900

    회원가입시 로그인아이디 중복체크

--- src/com/KoreaIT/JP/Main.java
+++ src/com/KoreaIT/JP/Main.java
@@ -55,6 +55,19 @@ public class Main {
 							System.out.println("아이디를 입력해주세요");
 							continue;
 						}
+
+						SecSql sql = new SecSql();
+						sql.append("SELECT COUNT(*) > 0");
+						sql.append("FROM `member`");
+						sql.append("WHERE loginId = ?", loginId);
+
+						boolean isLoginIdDup = DBUtil.selectRowBooleanValue(conn, sql);
+
+						if (isLoginIdDup) {
+							System.out.printf("%s는 이미 사용중인 아이디입니다\n", loginId);
+							continue;
+						}
+
 						break;
 					}
 					while (true) {
